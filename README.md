f09bf7d75532ece40bf576cf3679d5abd56f168f
ethiry99 <eric.thiry99@gmail.com>
2021-09-18 15:33:51 -0500

    completed readme

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# HW13_Mapping_Earthquakes
+## HW13_Mapping_Earthquakes
+
+#### Deliverable 1
+
+* Create a 2nd layer for the tectonic plate GeoJson to display
+* Add a reference to overlay object so the layer can be toggled on and off
+* Read in the boundaries from the given GitHub repo
+* Pass the tectonic plate data to the new layer
+* Style the lines with color and weight
+* Add the layer to the map
+* Add the layer to the layer group
+
+#### Deliverable 2
+
+* Create a 3rd layer for major earthquake data
+* Add a reference to overlay object so the layer can be toggled on and off
+* Read in the major earthquake data from the USGS 
+* Style the markers by size and color according to magnitude
+* Add the layer to the map
+* Add the layer to the layer group
+
+#### Deliverable 3
+
+* Add a 3rd map style
+* Include the 3rd style in the base layer object
+ 
--- static/js/challenge_logic.js
+++ static/js/challenge_logic.js
@@ -15,6 +15,13 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
 	accessToken: API_KEY
 });
 
+// We create the second tile layer that will be the background of our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
+
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
 	center: [40.7, -94.5],
@@ -25,7 +32,9 @@ let map = L.map('mapid', {
 // Create a base layer that holds all three maps.
 let baseMaps = {
   "Streets": streets,
-  "Satellite": satelliteStreets
+  "Satellite": satelliteStreets,
+  "Dark":dark
+
 };
 
 // 1. Add a 2nd layer group for the tectonic plate data.
@@ -130,10 +139,10 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geoj
 
   // This function determines the color of the marker based on the magnitude of the earthquake.
   function getColor(magnitude) {
-    if (magnitude > 5.5) {
+    if (magnitude > 6) {
       return "#ea2c2c";
     }
-    if (magnitude > 5) {
+    if (magnitude >= 5) {
       return "#ea822c";
     }
     
